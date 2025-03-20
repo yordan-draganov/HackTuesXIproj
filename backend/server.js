855c13e5a1a416c3f7e1683d609835c4652b36f5
Skele-ton <nathanielnechevski@gmail.com>
2025-03-20 12:34:10 +0200

    Add boilerplate app

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const port = 5000;
+const path = require("path");
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+// API Routes
+app.get("/api/message", (req, res) => {
+	res.json({ message: "boop" });
+});
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
+app.listen(port, () => {
+	console.log(`Server running on http://localhost:${port}`);
+});

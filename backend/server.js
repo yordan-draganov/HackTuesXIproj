f812517d6ad8fa4ab85745fa7e9091bf8841f248
Skele-ton <nathanielnechevski@gmail.com>
2025-03-20 14:47:59 +0200

    Add backend template before AI integration

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,28 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
 // API Routes
-app.get("/api/message", (req, res) => {
+app.get("/api/word", (req, res) => {
 	res.json({ word: "boop" });
 });
 
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
+
+app.post("/api/prompt", async (req, res) => {
+	const { moneyToInvest, numberOfDays, numberOfAssests, profitGoal } =
+		req.body;
+
+	console.log(
+		"Received data:",
+		moneyToInvest,
+		numberOfDays,
+		numberOfAssests,
+		profitGoal
+	);
+
+	// const result = `uno:${moneyToInvest}   dos:${numberOfDays}      tres:${numberOfAssests}     quatro:${profitGoal}`;
+	res.json({ result });
 });
 
 app.listen(port, () => {
